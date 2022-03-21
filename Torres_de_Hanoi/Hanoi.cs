02655eb7975479d3d25d80c8c75bf9c0a9b165bf
danialfaro <infdalfamosni@gmail.com>
2022-03-21 01:08:50 +0100

    Implementado método de mover disco

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -8,10 +8,36 @@ namespace Torres_de_Hanoi
 {
     class Hanoi
     {
-        /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
-        {
+        private int n;
+        private Pila ini, aux, fin;
 
+        public Hanoi(int n)
+        {
+            this.n = n;
+            ini = new Pila("INI");
+            aux = new Pila("AUX");
+            fin = new Pila("FIN");
+
+            for (int i = n; i > 0; i--)
+            {
+                ini.push(new Disco(i));
+            }
+        }
+
+        /*TODO: Implementar métodos*/
+        public bool mover_disco(Pila a, Pila b)
+        {
+            if (a.Top != null && (b.Top == null || a.Top.Valor < b.Top.Valor))
+            {
+                b.push(a.pop());
+                return true;
+            }
+            else if (b.Top != null && (a.Top == null || b.Top.Valor < a.Top.Valor))
+            {
+                a.push(b.pop());
+                return true;
+            }
+            return false;
         }
 
         public int iterativo(int n, Pila ini, Pila fin, Pila aux)
